3fc0bface2f5cf96359247d4c678eb9b711698c8
Jihyun Na <99661388+hyunzip@users.noreply.github.com>
2023-06-16 10:59:32 +0900

    edit js

--- static/js/ui.common.js
+++ static/js/ui.common.js
@@ -6,21 +6,20 @@ $(function(){
 
 
 	// btn-top 상단으로 이동
-	$(window).scroll(function(){
-		if($(this).scrollTop() > 100) {
-			$('.btn-top').fadeIn();
-		} else {
-			$('.btn-top').fadeOut();
-		}
-	});
+	// $(window).scroll(function(){
+	// 	if($(this).scrollTop() > 100) {
+	// 		$('.btn-top').fadeIn();
+	// 	} else {
+	// 		$('.btn-top').fadeOut();
+	// 	}
+	// });
 
-	$('.btn-top').click(function(e){
-		$('html, body').animate({scrollTop:0},1000);
-		e.preventDefault();
-	});
+	// $('.btn-top').click(function(e){
+	// 	$('html, body').animate({scrollTop:0},1000);
+	// 	e.preventDefault();
+	// });
 
 	// 스크롤 시 헤더 고정
-
 	$(document).scroll(function () {
 		if($(this).scrollTop() > 100) {
 			$('#header').addClass('on');
@@ -29,8 +28,7 @@ $(function(){
 		}
 	});
 
-	  // 헤더 메뉴 스크롤 연동
-
+	  // 헤더 메뉴 & 컨텐츠 스크롤 연동
 	  $(".menu li").each(function() {
         $(this).click(function(e) {
             var thisOffset = $("." + $(this).data('id')).offset().top -50;
